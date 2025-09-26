53e3379ce8542271e2ef27ad18a89a903619ddb8
Devaraj <devarajpd822@gmail.com>
2025-09-26 23:04:20 +0530

    app.blade changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,18 +11,7 @@
 <body>   
 
     <div class="container">
-        <h3 class=" mt-3">Simple Laravel 11 CRUD Application Tutorial - <a href="https://www.allphptricks.com/">AllPHPTricks.com</a></h3>
-            @yield('content')
-            <div class="row justify-content-center text-center mt-3">
-                <div class="col-md-12">
-                    <p>Back to Tutorial: 
-                        <a href="https://www.allphptricks.com/simple-laravel-11-crud-application-tutorial/"><strong>Tutorial Link</strong></a>
-                    </p>
-                    <p>
-                        For More Web Development Tutorials Visit: <a href="https://www.allphptricks.com/"><strong>AllPHPTricks.com</strong></a>
-                    </p>
-                </div>
-            </div>
+        @yield('content')
     </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
